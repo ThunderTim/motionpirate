5cceadb18998a2f288bfdd7640f212aa4ef2b560
Tim Coleman || ThunderFarm <tim@thunder.love>
2025-03-05 00:54:44 -0500

    work content update
    work content / styling

--- js/project-detail.js
+++ js/project-detail.js
@@ -425,13 +425,13 @@ if (project.credits && Object.keys(project.credits).length > 0) {
 }
 
         // Add share button
-const shareButtonContainer = document.createElement('div');
-shareButtonContainer.className = 'share-button-container';
+    const shareButtonContainer = document.createElement('div');
+    shareButtonContainer.className = 'share-button-container';
 
-const shareButton = document.createElement('button');
-shareButton.className = 'share-button';
-shareButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></svg> Share';
-shareButton.addEventListener('click', function() {
+    const shareButton = document.createElement('button');
+    shareButton.className = 'share-button';
+    shareButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path><polyline points="16 6 12 2 8 6"></polyline><line x1="12" y1="2" x2="12" y2="15"></line></svg> Share';
+    shareButton.addEventListener('click', function() {
     // Create the shareable link
     const projectUrl = window.location.origin + window.location.pathname + '?id=' + project.id;
     
